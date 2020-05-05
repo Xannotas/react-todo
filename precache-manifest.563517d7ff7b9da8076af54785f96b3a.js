42952dacf3161baa5a29d773c6c925f23102cf89
xannotas <xannotas@gmail.com>
2020-05-05 20:39:04 +0700

    Updates

---
+++ precache-manifest.563517d7ff7b9da8076af54785f96b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64feaaf20a8559ce24dd67ee2e02673c",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "cd91770cfaa6cad54af3",
+    "url": "/react-todo/static/css/main.1e7c4109.chunk.css"
+  },
+  {
+    "revision": "871f06e4b365c5ede3ae",
+    "url": "/react-todo/static/js/2.6738b325.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-todo/static/js/2.6738b325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd91770cfaa6cad54af3",
+    "url": "/react-todo/static/js/main.7e5a61e8.chunk.js"
+  },
+  {
+    "revision": "a7fb28e01dcc192be58f",
+    "url": "/react-todo/static/js/runtime-main.59b49ada.js"
+  },
+  {
+    "revision": "244e20008d74aff60d94697dd9f641c6",
+    "url": "/react-todo/static/media/list.244e2000.svg"
+  }
+]);
