3ee8ef328f272a81c1ec2769913284937f54ea51
xannotas <xannotas@gmail.com>
2020-05-05 19:48:55 +0700

    Хранение данных в localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
 
 import './styles/style.scss';
 
 import Sidebar from './components/Sidebar/Sidebar';
 import Content from './components/Content/Content'
+import { Folder } from './types';
+import { RootState } from './redux/store';
 
+type DispatchProps = {}
+type OwnProps = {}
+type StateProps = {
+  folders: Folder[],
+  currentFolderId: number | null,
+  isShowAllFolders: boolean
+}
+type Props = StateProps & DispatchProps & OwnProps
 
-const App: React.FC = () => {
+const App: React.FC<Props> = ({folders, currentFolderId, isShowAllFolders}) => {
+  useEffect(() => {
+    localStorage.setItem('folders', JSON.stringify(folders))
+  }, [folders])
+
+  useEffect(() => {
+    localStorage.setItem('currentFolderId', currentFolderId !== null ? currentFolderId.toString() : '')
+  }, [currentFolderId])
+
+  useEffect(() => {
+    localStorage.setItem('isShowAllFolders', isShowAllFolders ? 'true' : 'false')
+  }, [isShowAllFolders])
+
   return (
     <div className='app'>
       <Sidebar />
@@ -15,4 +38,12 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
+const mapState = (state: StateProps) => {
+  return {
+    folders: state.folders,
+    currentFolderId: state.currentFolderId,
+    isShowAllFolders: state.isShowAllFolders
+  }
+}
+
+export default connect<StateProps, DispatchProps, OwnProps, RootState>(mapState)(App);
